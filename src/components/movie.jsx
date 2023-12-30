a24350ddd09e69cade37aa287fc7c5784ce49523
Gani Madaris <dulab104@gmail.com>
2023-12-30 09:34:01 +0800

    adding search btn

--- src/components/movie.jsx
+++ src/components/movie.jsx
@@ -5,6 +5,16 @@ const baseUrlImg = import.meta.env.VITE_IMGURL
 
 const Movies =  () => {
     const [popularMovie, setPopularMovie] = useState([])
+    const [inputValue, setInputValue] = useState('')
+
+    const handleChange = (e) => {
+        setInputValue(e)
+    }
+    const handleClick = (inputvalue) => {
+        searchMovie(inputValue).then(r => {
+            setPopularMovie(r)
+        })
+    }
     useEffect( () => {
         getMovie().then(r => {
             setPopularMovie(r)
@@ -31,12 +41,12 @@ const Movies =  () => {
         } else {
             return popularMovie.map((m,i)=> {
             return (
-            <div key={i} className='w-64 h-[475px] bg-transparent border-2 p-3 rounded-lg'>
-                <img src={m.poster_path != null ? `${baseUrlImg}${m.poster_path}` : './../../public/default.png'} alt="movie img" className='rounded-lg mb-3' />
+            <button key={i} className='w-44 h-[375px] bg-transparent p-3 rounded-lg text-start flex flex-col justify-start border-2 group'>
+                <img src={m.poster_path != null ? `${baseUrlImg}${m.poster_path}` : './../../public/default.png'} alt="movie img" className='rounded-lg mb-3 group-hover:scale-110 transition duration-300' />
                 <h1 className='text-sm font-bold'>{m.title}</h1>
                 <p>release: {m.release_date}</p>
                 <h4>{`⭐${m.vote_average}`}</h4>
-            </div>
+            </button>
             )
     
             })   
@@ -48,10 +58,11 @@ const Movies =  () => {
       <h1 className='text-4xl font-bold align-middle mb-4'>Gani MovieDB</h1>
         <div className='w-full flex justify-center items-center'>
         {/* Search Bar */}
-          <input type="text" name="search" id="search" placeholder='Search Movie Here...' onChange={({target}) => searchMov(target.value) } className='border rounded-lg shadow-md px-3 py-2' />
+          <input type="text" name="search" id="search" placeholder='Search Movie Here...' onChange={({target}) => handleChange(target.value)} className='border rounded-lg rounded-r-none shadow-md outline-none px-3 py-2' />
+          <button className='border rounded-lg rounded-l-none shadow-md px-3 py-2 bg-sky-500' onClick={() => handleClick(inputValue)}>Search</button>
         </div>
     {/* Movie Container */}
-      <div className="w-full flex flex-wrap gap-3 justify-center items-center pt-5">
+      <div className="w-full flex flex-wrap justify-center  items-center pt-5">
         <PerMovie />
       </div>
     </div>
