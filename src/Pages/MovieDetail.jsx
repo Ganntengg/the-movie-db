0cce15d7b95055e2c2076aff1d0dfb430ede78a7
Gani Madaris <dulab104@gmail.com>
2024-01-01 19:11:57 +0800

    adding back button

--- src/Pages/MovieDetail.jsx
+++ src/Pages/MovieDetail.jsx
@@ -2,8 +2,8 @@ import MovieDetails from '../components/movieDetail/movieDetails'
 
 export default function MovieDetail() {
     return (
-        <>
+        <div className='bg-slate-900'>
             <MovieDetails />
-        </>
+        </div>
     )
 }
