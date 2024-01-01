f5604b4659f8cc1e2bc8fe6350a1d8a717a39570
Gani Madaris <dulab104@gmail.com>
2024-01-01 19:11:15 +0800

    adding some changes

--- tailwind.config.js
+++ tailwind.config.js
@@ -9,6 +9,12 @@ export default {
       aspectRatio: {
         portrait: "2/3",
       },
+      keyframes: {
+        goLeft: {
+          "0%, 100%": { translate: "0% 0px" },
+          "50%": { translate: "-20% 0px" },
+        },
+      },
     },
   },
   plugins: [],
