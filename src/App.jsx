2e0fdedaa044b9d52cbd39a42a698737f8e259e8
Gani Madaris <dulab104@gmail.com>
2024-01-02 12:14:25 +0800

    adding search and default route

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,16 @@
 import {BrowserRouter as Router, Routes, Route} from 'react-router-dom'
 import Home from './Pages/HomePage'
 import MovieDetail from './Pages/MovieDetail'
+import Search from './Pages/Search'
 
 function App() {
   return (
     <Router>
       <Routes>
         <Route path='/' element={<Home />} />
-        {/* <Route path='/popular' element={} /> */}
+        <Route path='/search?' element={<Search />} />
         <Route path="/movies/:id" element={<MovieDetail />} />
+        <Route path="/*" element={<h1>404</h1>} />
       </Routes>
     </Router>
   )
