f21bdbe4be03dbccd94b01c1e18b868fe1ad3291
Gani Madaris <dulab104@gmail.com>
2024-01-02 12:14:54 +0800

    adding some changes

--- src/components/imgSlider.jsx
+++ src/components/imgSlider.jsx
@@ -14,8 +14,6 @@ export default function ImageSlider() {
     getTopRatedMovie().then(r => {
         setTopRated(r)
     })
-    console.log(topRated)
-
 }, [])
 
 
@@ -25,10 +23,10 @@ export default function ImageSlider() {
                 {topRated.map((m,i) => {
                 return (
                     <Link key={i} to={`/movies/${m.id}`}>
-                        <h1 className='absolute z-10 text-white top-48 text-5xl left-24 font-bold'>{m.original_title}</h1>
-                        <h1 className='absolute z-10 text-white top-[16.1rem] text-3xl left-24'>{m.release_date}</h1>
-                        <h1 className='absolute z-10 text-white top-64 text-3xl left-72'><i className="fa-solid fa-star mr-2  text-yellow-400"></i>{`${Math.round(m.vote_average * 10) / 10}`}</h1>
-                        <h1 className='absolute z-10 text-white top-80 text-sm font-normal italic left-24'>{m.overview}</h1>
+                        <h1 className='absolute z-20 text-white top-48 text-5xl left-24 font-bold'>{m.original_title}</h1>
+                        <h1 className='absolute z-20 text-white top-[16.1rem] text-3xl left-24'>{m.release_date}</h1>
+                        <h1 className='absolute z-20 text-white top-64 text-3xl left-72'><i className="fa-solid fa-star mr-2  text-yellow-400"></i>{`${Math.round(m.vote_average * 10) / 10}`}</h1>
+                        <h1 className='absolute z-20 text-white top-80 text-sm font-normal italic left-24'>{m.overview}</h1>
                         <img src={`${baseUrlImg}${m.backdrop_path}`} alt="" className='w-full h-[400px] origin-center object-cover brightness-[.3] shadow-lg' />
                     </Link>
                 )
